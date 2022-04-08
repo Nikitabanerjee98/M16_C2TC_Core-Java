704ba0c7a2fe8a2f2fd727c2a1e2669815851da0
Nikita <Nikita@DESKTOP-ERTFKKH>
2022-04-08 23:52:48 +0530

    first commit

---
+++ Day2 introduction to java/src/com/in/cg/intro/Example1.java
@@ -0,0 +1,24 @@
+package com.in.cg.intro;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=15;
+		float b=10.5f;
+		double c=14.7;
+		char d='X';
+		String str="Nikita Banerjee";
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(str);
+
+
+
+
+
+	}
+
+}
